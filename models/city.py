cbf8705aa5f87ff80271235245a4f0fb6b684364
= <=>
2024-02-10 13:08:22 -0800

    test

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """Defines the City class."""
 from models.base_model import BaseModel
 
+
 class City(BaseModel):
     """City class"""
     state_id = ""
--- models/review.py
+++ models/review.py
@@ -2,6 +2,7 @@
 """Defines the Review class."""
 from models.base_model import BaseModel
 
+
 class Review(BaseModel):
     """Review class"""
     place_id = ""
