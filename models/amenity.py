7e70c06d512b601846ff72c1212145b313e60f49
= <=>
2024-02-10 12:55:12 -0800

    test

--- models/amenity.py
+++ models/amenity.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""Defines the Amenity class."""
 from models.base_model import BaseModel
 
 class Amenity(BaseModel):
--- models/review.py
+++ models/review.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""Defines the Review class."""
 from models.base_model import BaseModel
 
 class Review(BaseModel):
